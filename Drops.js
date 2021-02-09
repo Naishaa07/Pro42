f687012223a6b9b4b0cc351f4fc2315814c4a167
Naishaa07 <72614914+Naishaa07@users.noreply.github.com>
2021-02-09 18:57:20 +0530

    Add files via upload

---
+++ Drops.js
@@ -0,0 +1,24 @@
+class Drops{
+constructor(x,y,r){
+    var options={
+        friction: 0.001,
+        restitution:0.1   
+    }
+    this.r=r
+    this.body= Bodies.circle(x,y,this.r,options)
+    World.add(world, this.body)
+}
+display(){
+   
+      fill("blue");
+      ellipseMode(RADIUS);
+      ellipse(this.body.position.x,this.body.position.y, this.r, this.r);
+      
+    }
+    abc(){
+        if(this.body.position.y > height){
+            Matter.Body.setPosition(this.body,{x:random(0,400), y:random(0,400)})
+        }
+    }
+    
+}
